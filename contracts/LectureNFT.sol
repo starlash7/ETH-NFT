f9f73a6a5d7a486c2036640fd6b7487195bbc881
starlash7 <yang92sw@gmail.com>
2024-11-02 16:27:44 +0900

    NFT contract

--- contracts/LectureNFT.sol
+++ contracts/LectureNFT.sol
@@ -2,17 +2,31 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LectureNFT is ERC721 {
-    constructor() ERC721("LectureNFT", "LEC") {
-        for(uint i = 0; i < 10; i++) {
-            _safeMint(msg.sender, i + 1);
-        }
-    }
+contract LectureNFT is ERC721, Ownable {
+    uint256 private constant MAX_SUPPLY = 100;
+    bool public isSaleActive;
+    uint256 public totalSupply;
 
-    function setBaseURI(string memory uri) external onlyOwner{}
-
+    constructor() ERC721("LectureNFT", "LEC") Ownable(msg.sender) {}
+    
     function _baseURI() internal view override returns(string memory) {
         return "ipfs://Qmb64RAbojNAHfTWhkWMaP2JYZRaZwPr26ZTEhC9hGePDv";
+    }
+
+    function setSale(bool active) external onlyOwner {
+        isSaleActive = active;
+    }
+
+    function mintPlanet(uint256 count) external payable {
+        require(isSaleActive, "not on sale");
+        require(msg.value >= 10000000000000 * count);
+        require(count <= 10, "mint maximum 10 nfts at once");
+
+        for( uint i = 0; i < count; i++) {
+            require(totalSupply < MAX_SUPPLY, "max supply exceeded");
+            _safeMint(msg.sender, totalSupply++);
+        }
     }
 }
